8e898b8037b6b8f61d26fe57d4dc47eddaacdefe
MuhammadQumailRaza112 <mqumailr@gmail.com>
2022-03-06 06:29:32 +0500

    search filter

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
-import React, { useState } from 'react'
-import './App.css'
-import 'h8k-components'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "h8k-components";
 
-import { Movieform, Movieslist, Search } from './components'
+import { Movieform, Movieslist, Search } from "./components";
 
-const title = 'Favorite Movie Directory'
+const title = "Favorite Movie Directory";
 
 function App() {
-
-  const [movieList, setMovieList] = useState([])
+  const [movieList, setMovieList] = useState([]);
+  const [search, setSearch] = useState("");
 
   const addMovie = (movie) => {
-    let tempMovie = [...movieList]
+    let tempMovie = [...movieList];
     tempMovie.push(movie);
-    setMovieList(tempMovie)
-  }
+    setMovieList(tempMovie);
+  };
 
   return (
     <div>
-      <h8k-navbar header={ title } />
-      <div className='layout-row justify-content-center mt-100'>
-        <div className='w-30 mr-75'>
-          <Movieform  addMovie={addMovie}/>
+      <h8k-navbar header={title} />
+      <div className="layout-row justify-content-center mt-100">
+        <div className="w-30 mr-75">
+          <Movieform addMovie={addMovie} />
         </div>
-        <div className='layout-column w-30'>
-          <Search />
-          {
+        <div className="layout-column w-30">
+          <Search setSearch={setSearch} />
+          {search.length >= 2 ? (
+            movieList.filter((movie) => movie.name.startsWith(search)).length >
+            0 ? (
+              movieList
+                .filter((movie) => movie.name.startsWith(search))
+                .map((movie, index) => (
+                  <Movieslist key={index} movieData={movie} />
+                ))
+            ) : (
+              <div data-testid="noResult">
+                <h3 className="text-center">No Results Found</h3>
+              </div>
+            )
+          ) : (
             movieList.map((movie, index) => (
-              <Movieslist key={index} movieData={movie} /> 
+              <Movieslist key={index} movieData={movie} />
             ))
-          }
-          <div data-testid='noResult'>
-            <h3 className='text-center'>No Results Found</h3>
-          </div>
+          )}
         </div>
-      </div> 
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useRef } from "react";
 
-function Search() {
+function Search({ setSearch }) {
+  const ref = useRef();
 
   return (
-    <section className='layout-row justify-content-center mb-40'>
-      <input 
-        type='text'
-        placeholder='Search for movie by name' 
-        className='w-75 py-2'
-        data-testid='search'
+    <section className="layout-row justify-content-center mb-40">
+      <input
+        ref={ref}
+        type="text"
+        placeholder="Search for movie by name"
+        className="w-75 py-2"
+        data-testid="search"
+        onChange={(e) => setSearch(e.target.value)}
       />
     </section>
-  )
+  );
 }
 
-export default Search
+export default Search;
