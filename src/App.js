ff8064b406b2c13f60dc9c7ffa0ee2e7b122ff8a
MuhammadQumailRaza112 <mqumailr@gmail.com>
2022-03-06 06:12:52 +0500

    add prortfolio and list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import 'h8k-components'
 
@@ -8,16 +8,28 @@ const title = 'Favorite Movie Directory'
 
 function App() {
 
+  const [movieList, setMovieList] = useState([])
+
+  const addMovie = (movie) => {
+    let tempMovie = [...movieList]
+    tempMovie.push(movie);
+    setMovieList(tempMovie)
+  }
+
   return (
     <div>
       <h8k-navbar header={ title } />
       <div className='layout-row justify-content-center mt-100'>
         <div className='w-30 mr-75'>
-          <Movieform />
+          <Movieform  addMovie={addMovie}/>
         </div>
         <div className='layout-column w-30'>
           <Search />
-          <Movieslist /> 
+          {
+            movieList.map((movie, index) => (
+              <Movieslist key={index} movieData={movie} /> 
+            ))
+          }
           <div data-testid='noResult'>
             <h3 className='text-center'>No Results Found</h3>
           </div>
--- src/components/Movieform.js
+++ src/components/Movieform.js
@@ -1,58 +1,104 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-function Movieform() {
+function Movieform({addMovie}) {
+  const DurationRef = useRef();
+  const nameRef = useRef();
+  const ratingRef = useRef();
+  const [showError, setShowError] = useState(false);
+
+  const resetForm = () => {
+     DurationRef.current.value = null
+     nameRef.current.value = null
+     ratingRef.current.value = null
+  }
+
+  const checkValid = (value) => {
+    const trimed = value.slice(0, -1);
+    var er = /^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/;
+    const isValid = er.test(trimed);
+    if (!isValid || (value[value.length - 1] != 'h' && value[value.length - 1] != 'm') ) {
+      setShowError(true);
+      return false
+    }
+    return true
+  };
+
+  const handleAdd = () => {
+    const isValid = checkValid(DurationRef.current.value)
+    if(isValid && nameRef.current.value && ratingRef.current.value){
+      addMovie({
+        name: nameRef.current.value,
+        rating: ratingRef.current.value,
+        duration: DurationRef.current.value
+      })
+    resetForm()
+
+    }
+  }
 
   return (
     <section>
-      <div className='card pa-30'>
-        <form onSubmit={ e => e.preventDefault() }>
-          <div className='layout-column mb-15'>
-            <label htmlFor='name' className='mb-3'>Movie Name</label>
-            <input 
-              type='text' 
-              id='name'
-              placeholder='Enter Movie Name'
-              data-testid='nameInput'
+      <div className="card pa-30">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="layout-column mb-15">
+            <label htmlFor="name" className="mb-3">
+              Movie Name
+            </label>
+            <input
+            ref={nameRef}
+              type="text"
+              id="name"
+              placeholder="Enter Movie Name"
+              data-testid="nameInput"
             />
           </div>
-          <div className='layout-column mb-15'>
-            <label htmlFor='ratings' className='mb-3'>Ratings</label>
-            <input 
-              type='number' 
-              id='ratings'
-              placeholder='Enter Rating on a scale of 1 to 100'
-              data-testid='ratingsInput'
+          <div className="layout-column mb-15">
+            <label htmlFor="ratings" className="mb-3">
+              Ratings
+            </label>
+            <input
+            ref={ratingRef}
+              type="number"
+              id="ratings"
+              placeholder="Enter Rating on a scale of 1 to 100"
+              data-testid="ratingsInput"
             />
           </div>
-          <div className='layout-column mb-30'>
-            <label htmlFor='duration' className='mb-3'>Duration</label>
-            <input 
-              type='text' 
-              id='duration'
-              placeholder='Enter duration in hours or minutes'
-              data-testid='durationInput'
+          <div className="layout-column mb-30">
+            <label htmlFor="duration" className="mb-3">
+              Duration
+            </label>
+            <input
+              defaultValue={""}
+              ref={DurationRef}
+              type="text"
+              id="duration"
+              placeholder="Enter duration in hours or minutes"
+              data-testid="durationInput"
+              onFocus={() => setShowError(false)}
             />
           </div>
           {/* Use this div when time format is invalid */}
-          {/* <div 
-            className='alert error mb-30'
-            data-testid='alert'
-          >
-            Please specify time in hours or minutes (e.g. 2.5h or 150m)
-          </div>  */}
-          <div className='layout-row justify-content-end'>
-            <button 
-              type='submit'
-              className='mx-0'
-              data-testid='addButton'
+          {showError && (
+            <div className="alert error mb-30" data-testid="alert">
+              Please specify time in hours or minutes (e.g. 2.5h or 150m)
+            </div>
+          )}
+
+          <div className="layout-row justify-content-end">
+            <button
+              onClick={handleAdd}
+              type="submit"
+              className="mx-0"
+              data-testid="addButton"
             >
               Add Movie
             </button>
           </div>
-          </form>
-      </div> 
+        </form>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Movieform
+export default Movieform;
